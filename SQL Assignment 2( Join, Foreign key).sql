22506ee12d3b9fe26847b1099f165d498fef000a
adityashakya07 <shakyaaditya911@gmail.com>
2026-04-06 22:45:23 +0530

    Update SQL Assignment 2( Join, Foreign key).sql

--- SQL Assignment 2( Join, Foreign key).sql
+++ SQL Assignment 2( Join, Foreign key).sql
@@ -89,3 +89,57 @@ on e.dept_id = d.dept_id
 where salary>40000;
 SELECT dept_id, MAX(salary) FROM Employees
 GROUP BY dept_id;
+
+SELECT e.*
+FROM Employees e
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM Employees
+    WHERE dept_id = e.dept_id
+);
+SELECT dept_id
+FROM Employees
+GROUP BY dept_id
+HAVING AVG(salary) > 45000;
+SELECT *
+FROM Employees
+WHERE dept_id = (
+    SELECT dept_id FROM Employees WHERE emp_name = 'Amit'
+);
+SELECT p1.emp_id, p2.emp_id, p1.project_id
+FROM Projects p1
+JOIN Projects p2 
+ON p1.project_id = p2.project_id AND p1.emp_id <> p2.emp_id;
+SELECT *
+FROM Employees e
+LEFT JOIN Projects p ON e.emp_id = p.emp_id
+WHERE p.project_id IS NULL AND e.dept_id IS NOT NULL;
+SELECT dept_id, SUM(salary), AVG(salary)
+FROM Employees
+GROUP BY dept_id;
+SELECT *
+FROM Employees
+WHERE salary = (SELECT MAX(salary) FROM Employees);
+SELECT d.dept_name
+FROM Departments d
+LEFT JOIN Employees e ON d.dept_id = e.dept_id
+LEFT JOIN Projects p ON e.emp_id = p.emp_id
+WHERE p.project_id IS NULL;
+ALTER TABLE Employees
+DROP FOREIGN KEY Employees_ibfk_1;
+
+ALTER TABLE Employees
+ADD FOREIGN KEY (dept_id)
+REFERENCES Departments(dept_id)
+ON DELETE SET NULL;
+ALTER TABLE Employees
+DROP FOREIGN KEY Employees_ibfk_1;
+
+ALTER TABLE Employees
+ADD FOREIGN KEY (dept_id)
+REFERENCES Departments(dept_id)
+ON UPDATE CASCADE;
+ALTER TABLE Projects
+ADD CONSTRAINT fk_emp
+FOREIGN KEY (emp_id)
+REFERENCES Employees(emp_id);
